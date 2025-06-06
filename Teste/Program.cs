fcebcfd197f60f1ed0e4c8c87534e925c9f630c3
D_Martins21 <davi_martins2009@outlook.com>
2025-06-06 17:47:12 -0300

    "Resolvido"

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -1,45 +1,26 @@
-﻿Console.WriteLine("Qual a ordem da matriz?");
-int n = int.Parse(Console.ReadLine());
-int[,] matriz = new int[n, n];
+﻿Console.Write("Quantidade de linhas:");
+int l = int.Parse(Console.ReadLine());
+Console.Write("Quantidade de colunas:");
+int c = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < n; i++)
+int [,] matriz = new int [l,c];
+
+for (int i = 0; i < l; i++)
 {
-    for (int j = 0; j < n; j++)
+    Console.WriteLine($"Digite o valor da {i+1} linha:");
+    for (int j = 0; j < c; j++)
     {
-        Console.WriteLine($"Elemento {i}, {j}:");
         matriz[i, j] = int.Parse(Console.ReadLine());
     }
 }
 
-Console.WriteLine("Matriz Completa:");
-foreach (var elemento in matriz)
+Console.WriteLine("Vetor:");
+for (int i = 0; i < l; i++)
 {
-    Console.WriteLine(elemento);
-}
-
-Console.WriteLine("Diagonal Principal:");
-for (int i = 0; i < n; i++)
-{
-    Console.WriteLine($"{matriz[i, i]}");
-}
-
-int x = 0;
-
-foreach (var elemento in matriz)
-{
-    if(elemento < 0)  x++;
-}
-
-/*for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
+    int vetor = 0;
+    for (int j = 0; j < c; j++)
     {
-        int negativos = matriz[i, j];
-        if (negativos < 0)
-        {
-            x++;
-        }
+        vetor += matriz[i, j];
     }
+    Console.WriteLine(vetor);
 }
-*/
-Console.WriteLine($"Quantidade de negativos: {x}");
