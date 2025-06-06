d7c7318f44ee061d3882245c11554327b1dab0b0
D_Martins21 <davi_martins2009@outlook.com>
2025-06-06 16:54:00 -0300

    "Primeiro Exercicio"

---
+++ Teste/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Qual a ordem da matriz?");
+int n = int.Parse(Console.ReadLine());
+int[,] matriz = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.WriteLine($"Elemento {i}, {j}:");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Matriz Completa:");
+foreach (var elemento in matriz)
+{
+    Console.WriteLine(elemento);
+}
+
+Console.WriteLine("Diagonal Principal:");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"{matriz[i, i]}");
+}
+
+int x = 0;
+
+foreach (var elemento in matriz)
+{
+    if(elemento < 0)  x++;
+}
+
+/*for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        int negativos = matriz[i, j];
+        if (negativos < 0)
+        {
+            x++;
+        }
+    }
+}
+*/
+Console.WriteLine($"Quantidade de negativos: {x}");
